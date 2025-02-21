7304249ed6b727aacf88d737599139cb28a60d98
Nikhil-Varanasi <nikhilvaranasi111@gmail.com>
2025-02-21 08:29:34 +0530

    rds code

---
+++ 40-rds/variables.tf
@@ -0,0 +1,21 @@
+variable "project_name" {
+    default = "expense"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "common_tags" {
+    default = {
+        project = "expense"
+        Terraform = "true"
+        Environment = "dev"
+    }
+}
+
+variable "rds_tags" {
+    default = {
+        component = "mysql"
+    }
+}
